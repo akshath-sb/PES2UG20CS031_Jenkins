474846242943dd156ebe18c434e57635f4b44c3f
akshath-sb <83830261+akshath-sb@users.noreply.github.com>
2023-02-17 17:57:45 +0530

    Update PES2UG20CS031.cpp

--- PES2UG20CS031.cpp
+++ PES2UG20CS031.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "check to see if this works PES2UG20CS031\n";
+  cout << "check to see if this works PES2UG20CS031\n"
   return 0;
 }
